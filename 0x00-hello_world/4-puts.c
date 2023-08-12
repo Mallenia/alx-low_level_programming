643034a2e07c00601683a235f64fff20f1fee950
Mallenia <malleniajenetoh@gmail.com>
2023-08-12 16:29:28 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char Prog[52];
+
+	strcpy(Prog, "Programming is like building a multilingual puzzle\n");
+
+	puts(Prog);
+
+	return (0);
+}
+
