d63ee7ead992d6fb62ec2a8a95ebdab68e4ee401
Mallenia <malleniajenetoh@gmail.com>
2023-08-16 00:49:26 -0700

    0. Multiple mains Task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main = A function that checks postitive_or_negative for 0
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	/* This statemenet checks if i is zero*/
+	if (i == 0)
+		printf("i is zero\n");
+
+	return (0);
+}
