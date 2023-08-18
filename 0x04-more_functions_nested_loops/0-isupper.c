7a36a6f368ac13b6063cb1a63549fca2abd44061
Mallenia <malleniajenetoh@gmail.com>
2023-08-17 23:20:33 -0700

    0. isupper Task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Prints 1 if the aphabet is upper case
+ *and 0 otherwise
+ *@c: The alphabet that is checked
+ * Return: 1 if the alphabet is upper case
+ */
+
+int _isupper(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
