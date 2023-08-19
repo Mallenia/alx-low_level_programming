f79ed3bd6aec9bf5ca0520ac13c4de20b3861174
Mallenia <malleniajenetoh@gmail.com>
2023-08-18 23:34:02 -0700

    Triangles Task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of a given size
+ * @size: The size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size > 0)
+	{
+		for (k = 1; k <= size; k ++)
+		{
+			for (i = size - k; i > 0; --i)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= k; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}	
