9f2abaf3df10b3247fb0dc9232fe58fc72f439d8
Mallenia <malleniajenetoh@gmail.com>
2023-08-21 01:40:37 -0700

    98 Battery st. Task

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Printout a character
+ *@a: Charecter to be printed
+ * Return: 0 (Success)
+ */
+
+int _putchar(char a)
+{
+	write(1, &a, 1);
+
+	return (0);
+}
